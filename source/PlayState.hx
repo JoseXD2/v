001c12b45d825a6f8450c059e4e8281b46753909
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-02-25 07:44:37 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -453,7 +453,39 @@ class PlayState extends MusicBeatState
 					// var cityLights:FlxSprite = new FlxSprite().loadGraphic(AssetPaths.win0.png);
 
 					var street:FlxSprite = new FlxSprite(-40, streetBehind.y).loadGraphic(Paths.image('philly/street','week3'));
-					add(street);
+					add(street)
+			    }
+			// WITHERED FREDDY STAGE 1
+			case 'diningarea':
+				{
+					defaultCamZoom = 0.8;
+					curStage = 'diningarea';
+					var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('freddystage/stageback','shared'));
+					bg.setGraphicSize(Std.int(bg.width / 0.9));
+					
+					bg.x = -600; {
+					bg.y = -200;
+					add(bg);
+
+					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
+					balloons.setGraphicSize(Std.int(balloons.width * 0));
+					balloons.x = -580;
+					balloons.y = -200;
+					balloons.visible = true;
+					
+
+					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
+					balloons.setGraphicSize(Std.int(balloons.width * 0));
+					balloons.x = -580;
+					balloons.y = -200;
+					balloons.visible = true;
+					
+					
+					balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
+					balloons.setGraphicSize(Std.int(balloons.width * 0));
+					balloons.x = -580;
+					balloons.y = -200;
+					balloons.visible = true;
 			}
 			case 'limo':
 			{
@@ -740,39 +772,6 @@ class PlayState extends MusicBeatState
 						stageCurtains.active = false;
 	
 						add(stageCurtains);
-				}
-			// WITHERED FREDDY STAGE 1
-			case 'diningarea':
-				{
-				defaultCamZoom = 0.8;
-				curStage = 'diningarea';
-				var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('freddystage/stageback','shared'));
-				bg.setGraphicSize(Std.int(bg.width / 0.9));
-					
-				bg.x = -600; {
-				bg.y = -200;
-				add(bg);
-					
-				balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
-				balloons.setGraphicSize(Std.int(balloons.width * 0));
-				balloons.x = -580;
-				balloons.y = -200;
-				balloons.visible = true;
-				
-					
-				balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
-				balloons.setGraphicSize(Std.int(balloons.width * 0));
-				balloons.x = -580;
-				balloons.y = -200;
-				balloons.visible = true;
-	
-					
-				balloons = new FlxSprite().loadGraphic(Paths.image('freddystage/stagecurtains','shared'));
-				balloons.setGraphicSize(Std.int(balloons.width * 0));
-				balloons.x = -580;
-				balloons.y = -200;
-				balloons.visible = true;
-	
 				}
 			default:
 			{
